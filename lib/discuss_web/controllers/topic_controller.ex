4d917f5d1b370aa9935ecbfbaeed8512472c397b
Adelina-Chiriac <adelinachiriac@gmail.com>
2022-02-20 16:23:21 +0100

    Redirect user to index page & handle successful database insertion validation

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,8 +1,15 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
+  alias DiscussWeb.Router.Helpers, as: Routes
   alias DiscussWeb.Topic
   alias Discuss.Repo
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+
+    render(conn, "index.html", topics: topics)
+  end
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
@@ -16,7 +23,10 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, topic} ->
+        conn
+        |> put_flash(:info, "Topic created!")
+        |> redirect(to: Routes.topic_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
